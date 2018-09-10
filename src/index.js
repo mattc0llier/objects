cd9ce26863f44f61bdbb500511c79341559cf131
mattc0llier <matt.collier@hotmail.com>
2018-09-10 16:54:14 +0100

    answered tests up to getAnimalNooise

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ const getStaticBuilding = () => {
   // return an object representing a building
   // it should 2 properties: `numberOfFloors` and `numberOfWindows`
   // numberOfFloors should have the value 5 and numberOfWindows should have the value 8
-}
+
+  return {
+    numberOfFloors: 5,
+    numberOfWindows: 8
+  };
+};
 
 const getBuilding = (numberOfFloors, numberOfWindows) => {
   // function receives two parameters: numberOfFloors and numberOfWindows
   // return a building object with 2 properties: `numberOfFloors` and `numberOfFloors`
   // the values of the properties should be set from input parameters
-}
+  return {
+    numberOfFloors: numberOfFloors,
+    numberOfWindows: numberOfWindows
+  };
+};
 
 const getHeight = (mountains, name) => {
   // function receives two arguments: an object called mountains and a string called name
   // mountains contains mountain names as keys and their height as values
-
   // return the height of the mountain specified by name
-}
+  return mountains[name];
+};
 
 const getCar = () => {
   // return a new car object.
@@ -23,39 +32,69 @@ const getCar = () => {
   // The car should also have two methods `faster` and `slower`
   // `faster` should increase the speed property by 10
   // `slower` should decrease the speed property by 10
-}
+  return {
+    speed: 0,
+    faster() {
+      this.speed += 10;
+    },
+    slower() {
+      this.speed -= 10;
+    }
+  };
+};
 
 const getLimitedCar = topSpeed => {
   // function receives one parameter, an integer called topSpeed.
   // return a new car object with 2 properties: `currentSpeed` and `topSpeed`
   // currentSpeed should be initialised to 0
   // topSpeed should be initialised to the parameter passed in
-
   // The car should also have two methods `faster` and `slower`
   // `faster` should increase the speed property by 10, but only up to and including topSpeed
   // `slower` should decrease the speed property by 10, but never drop below 0
-}
+
+  return {
+    speed: 0,
+    topSpeed,
+    faster() {
+      console.log(this.speed);
+      console.log(this.topSpeed);
+      this.speed += 10;
+      if (this.speed > this.topSpeed) {
+        this.speed = this.topSpeed;
+      }
+    },
+    slower() {
+      this.speed -= 10;
+      if (this.speed < 0) {
+        this.speed = 0;
+      }
+    }
+  };
+};
 
 const getAnimalNoise = (farm, animalName) => {
   // the function receives two parameters: a farm object and a string which contains an animal name
   // the farm object has methods whose names are animal names. When called they return
   // the noise that animal makes.
-
   // If farm object contains a method with name passed in as animalName parameter, call the method and
   // return string output by the method.
   // Otherwise, if method does not exist, return the string 'No such animal here'
-}
+  console.log(farm[animalName]);
+  if (farm[animalName] !== undefined) {
+    return farm[animalName](animalName);
+  } else {
+    return "No such animal here";
+  }
+};
 
 const teaBagsLeft = teaBags => {
   // function receives an object where keys are names of tea types
   // and values are the number of teaBags we have of that type.
-
   // return the total number teabags we have left
-}
+};
 
 const convertBookArrayToMap = books => {
   // the function receives an array of books where each book has an id, author, title and year
-
   // for example
   // {
   //   id:1,
@@ -63,10 +102,8 @@ const convertBookArrayToMap = books => {
   //   title: "The count of Monte Christo",
   //   year: 1884
   // }
-
   // convert the array of books into a map, where each key is a bookId and the corresponding value
   // is the object from the array
-
   // for example
   // {
   //   1: {
@@ -76,7 +113,7 @@ const convertBookArrayToMap = books => {
   //     year: 1884
   //   }
   // }
-}
+};
 
 const dogCount = dogs => {
   // function receives an array of dogs, each dog has a name and location
@@ -85,10 +122,9 @@ const dogCount = dogs => {
   //     name: 'ruby',
   //     location: 'liverpool'
   // }
-
   // return an object which has locations as keys and
   // the number of dogs at that location as values
-}
+};
 
 const dogNames = dogs => {
   // function receives an array of dogs, each dog has a name and location
@@ -97,11 +133,10 @@ const dogNames = dogs => {
   //     name: 'ruby',
   //     location: 'liverpool'
   // }
-
   // return an object which has locations as keys and
   // as values an array which contains the names of the dogs
   // at that location
-}
+};
 
 const fruitMarket = boxes => {
   // function receives an array of fruit box objects.
@@ -111,10 +146,9 @@ const fruitMarket = boxes => {
   //   contents: 'apples',
   //   number: 10
   // }
-
   // return an object that has contents as keys and the total for that
   // fruit in all boxes as values.
-}
+};
 
 const averageFruitPerBox = boxes => {
   // function receives an array of fruit box objects.
@@ -124,36 +158,30 @@ const averageFruitPerBox = boxes => {
   //   contents: 'apples',
   //   number: 10
   // }
-
   // return an object that has contents as keys and the average number of
   // fruit per box for that fruit type as value
-}
+};
 
 /* STRETCH GOALS */
 
 const calculateOrderPrice = (menu, order) => {
   // function receives two parameters: `menu` and `order`
-
   // menu has item names as keys and prices as values
   // order has item names as keys and quantities as values
-
   // calculate a total cost for each order using quantities from `order` and prices from `menu`
-}
-
+};
 
 const calculateOrderPriceWithType = (menu, order, type) => {
   // function receives three parameters: `menu`, `order` and `type`
-
   // menu has item names as keys and prices as values
   // order has item names as keys and quantities as values
   // type is a string whose value is either `eatIn` or `takeAway`
-
   // calculate a total cost for each order using quantities from `order` and prices from `menu`
   // add 10% service charge for `eatIn` orders and a fixed delivery charge of 5 to `takeAway` orders
   // try to implement the service charge and delivery charge calculation without using if/else
   // hint: you can create an object with methods that calculate the charges and call the relevant one using
   // the order type provided
-}
+};
 
 module.exports = {
   getStaticBuilding,
@@ -170,4 +198,4 @@ module.exports = {
   averageFruitPerBox,
   calculateOrderPrice,
   calculateOrderPriceWithType
-}
+};
--- test/index.test.js
+++ test/index.test.js
@@ -13,9 +13,9 @@ const {
   averageFruitPerBox,
   calculateOrderPrice,
   calculateOrderPriceWithType
-} = require('../src');
+} = require("../src");
 
-test('Get static building', () => {
+test("Get static building", () => {
   const output = getStaticBuilding();
 
   expect(output).toEqual({
@@ -24,7 +24,7 @@ test('Get static building', () => {
   });
 });
 
-test('Get building', () => {
+test("Get building", () => {
   const output = getBuilding(3, 4);
 
   expect(output).toEqual({
@@ -33,170 +33,172 @@ test('Get building', () => {
   });
 });
 
-test('get height', () => {
+test("get height", () => {
   const mountains = {
     everest: 8848,
     k2: 8611,
     lhotse: 8586
   };
 
-  const output = getHeight(mountains, 'lhotse');
+  const output = getHeight(mountains, "lhotse");
 
   expect(output).toEqual(8586);
 });
 
-test('Get car', () => {
+test("Get car", () => {
   const car = getCar();
 
-  expect(car.speed).toEqual( 0 );
+  expect(car.speed).toEqual(0);
 
   car.faster();
   car.faster();
-  expect(car.speed).toEqual( 20 );
+  expect(car.speed).toEqual(20);
 
   car.slower();
-  expect(car.speed).toEqual( 10 );
+  expect(car.speed).toEqual(10);
 });
 
-test('Get limited car', () => {
+test("Get limited car", () => {
   const car = getLimitedCar(15);
 
-  expect(car.speed).toEqual( 0 );
+  expect(car.speed).toEqual(0);
 
   car.faster();
   car.faster();
-  expect(car.speed).toEqual( 15 );
+  expect(car.speed).toEqual(15);
 
   car.slower();
-  expect(car.speed).toEqual( 5 );
+  expect(car.speed).toEqual(5);
 
   car.slower();
-  expect(car.speed).toEqual( 0 );
+  expect(car.speed).toEqual(0);
 });
 
-test('get animal noise', () => {
-
+test.only("get animal noise", () => {
   const farm = {
-    sheep(){
-      return 'Baa';
+    sheep() {
+      return "Baa";
     },
-    cow(){
-      return 'Moo';
+    cow() {
+      return "Moo";
     },
-    dog(){
-      return 'Woof'
+    dog() {
+      return "Woof";
     }
-  }
-  const noise = getAnimalNoise(farm, 'cow');
+  };
+  const noise = getAnimalNoise(farm, "cow");
 
-  expect(noise).toEqual( 'Moo' );
+  expect(noise).toEqual("Moo");
 });
 
-test('get animal noise - 2', () => {
-
+test.only("get animal noise - 2", () => {
   const farm = {
-    sheep(){
-      return 'Baa';
+    sheep() {
+      return "Baa";
     },
-    cow(){
-      return 'Moo';
+    cow() {
+      return "Moo";
     },
-    dog(){
-      return 'Woof'
+    dog() {
+      return "Woof";
     }
-  }
-  const noise = getAnimalNoise(farm, 'chicken');
+  };
+  const noise = getAnimalNoise(farm, "chicken");
 
-  expect(noise).toEqual( 'No such animal here' );
+  expect(noise).toEqual("No such animal here");
 });
 
-test('tea bags', () => {
-
+test("tea bags", () => {
   const teaBags = {
-    'earlGrey': 12,
-    'rooibos': 23,
-    'matcha': 8,
-    'englishBreakfast': 17
+    earlGrey: 12,
+    rooibos: 23,
+    matcha: 8,
+    englishBreakfast: 17
   };
   const output = teaBagsLeft(teaBags);
 
-  expect(output).toEqual( 60 );
+  expect(output).toEqual(60);
 });
 
-test('Convert array to map', () => {
-  const books = [{
-      id:1,
+test("Convert array to map", () => {
+  const books = [
+    {
+      id: 1,
       author: "Alexander Dumas",
       title: "The count of Monte Christo",
       year: 1884
-  }, {
+    },
+    {
       id: 2,
       author: "Douglas Crockford",
       title: "JavaScript: The Good Parts",
       year: 2008
-  }, {
+    },
+    {
       id: 3,
       author: "JK Rowling",
       title: "Harry Potter and the Philosopher's Stone",
       year: 1997
-  }, {
-      id:4,
+    },
+    {
+      id: 4,
       author: "Arthur Conan Doyle",
       title: "The Adventures of Sherlock Holmes",
       year: 1892
-  }]
+    }
+  ];
 
   const output = convertBookArrayToMap(books);
 
   expect(output).toEqual({
     1: {
-        id:1,
-        author: "Alexander Dumas",
-        title: "The count of Monte Christo",
-        year: 1884
+      id: 1,
+      author: "Alexander Dumas",
+      title: "The count of Monte Christo",
+      year: 1884
     },
     2: {
-        id: 2,
-        author: "Douglas Crockford",
-        title: "JavaScript: The Good Parts",
-        year: 2008
+      id: 2,
+      author: "Douglas Crockford",
+      title: "JavaScript: The Good Parts",
+      year: 2008
     },
     3: {
-        id: 3,
-        author: "JK Rowling",
-        title: "Harry Potter and the Philosopher's Stone",
-        year: 1997
+      id: 3,
+      author: "JK Rowling",
+      title: "Harry Potter and the Philosopher's Stone",
+      year: 1997
     },
     4: {
-        id:4,
-        author: "Arthur Conan Doyle",
-        title: "The Adventures of Sherlock Holmes",
-        year: 1892
+      id: 4,
+      author: "Arthur Conan Doyle",
+      title: "The Adventures of Sherlock Holmes",
+      year: 1892
     }
-  })
+  });
 });
 
-test('dog count', () => {
+test("dog count", () => {
   const dogs = [
     {
-        name: 'ruby',
-        location: 'liverpool'
+      name: "ruby",
+      location: "liverpool"
     },
     {
-        name: 'max',
-        location: 'manchester'
+      name: "max",
+      location: "manchester"
     },
     {
-        name: 'woofie',
-        location: 'liverpool'
+      name: "woofie",
+      location: "liverpool"
     },
     {
-        name: 'snoopy',
-        location: 'leeds'
+      name: "snoopy",
+      location: "leeds"
     },
     {
-        name: 'wellard',
-        location: 'manchester'
+      name: "wellard",
+      location: "manchester"
     }
   ];
 
@@ -206,63 +208,63 @@ test('dog count', () => {
     liverpool: 2,
     manchester: 2,
     leeds: 1
-  })
+  });
 });
 
-test('dog names', () => {
+test("dog names", () => {
   const dogs = [
     {
-        name: 'ruby',
-        location: 'liverpool'
+      name: "ruby",
+      location: "liverpool"
     },
     {
-        name: 'max',
-        location: 'manchester'
+      name: "max",
+      location: "manchester"
     },
     {
-        name: 'woofie',
-        location: 'liverpool'
+      name: "woofie",
+      location: "liverpool"
     },
     {
-        name: 'snoopy',
-        location: 'leeds'
+      name: "snoopy",
+      location: "leeds"
     },
     {
-        name: 'wellard',
-        location: 'manchester'
+      name: "wellard",
+      location: "manchester"
     }
   ];
 
   const output = dogNames(dogs);
 
   expect(output).toEqual({
-    liverpool: ['ruby', 'woofie'],
-    manchester: ['max', 'wellard'],
-    leeds: ['snoopy']
-  })
+    liverpool: ["ruby", "woofie"],
+    manchester: ["max", "wellard"],
+    leeds: ["snoopy"]
+  });
 });
 
-test('fruit market', () => {
+test("fruit market", () => {
   const boxes = [
     {
-        contents: 'apples',
-        number: 10
+      contents: "apples",
+      number: 10
     },
     {
-        contents: 'oranges',
-        number: 8
+      contents: "oranges",
+      number: 8
     },
     {
-        contents: 'apples',
-        number: 26
+      contents: "apples",
+      number: 26
     },
     {
-        contents: 'pears',
-        number: 3
+      contents: "pears",
+      number: 3
     },
     {
-        contents: 'oranges',
-        number: 22
+      contents: "oranges",
+      number: 22
     }
   ];
 
@@ -274,27 +276,27 @@ test('fruit market', () => {
   });
 });
 
-test('averageFruitPerBox', () => {
+test("averageFruitPerBox", () => {
   const boxes = [
     {
-        contents: 'apples',
-        number: 10
+      contents: "apples",
+      number: 10
     },
     {
-        contents: 'oranges',
-        number: 8
+      contents: "oranges",
+      number: 8
     },
     {
-        contents: 'apples',
-        number: 26
+      contents: "apples",
+      number: 26
     },
     {
-        contents: 'pears',
-        number: 3
+      contents: "pears",
+      number: 3
     },
     {
-        contents: 'oranges',
-        number: 22
+      contents: "oranges",
+      number: 22
     }
   ];
 
@@ -306,7 +308,7 @@ test('averageFruitPerBox', () => {
   });
 });
 
-test('calculate order price', () => {
+test("calculate order price", () => {
   const menu = {
     hotdog: 8,
     burger: 10,
@@ -324,7 +326,7 @@ test('calculate order price', () => {
   expect(output).toEqual(29);
 });
 
-test('calculate order price with type - eat in', () => {
+test("calculate order price with type - eat in", () => {
   const menu = {
     hotdog: 8,
     burger: 10,
@@ -338,11 +340,11 @@ test('calculate order price with type - eat in', () => {
     cheeseCake: 4
   };
 
-  const output = calculateOrderPriceWithType(menu, order, 'eatIn');
+  const output = calculateOrderPriceWithType(menu, order, "eatIn");
   expect(output).toEqual(44);
 });
 
-test('calculate order price with type - take away', () => {
+test("calculate order price with type - take away", () => {
   const menu = {
     hotdog: 8,
     burger: 10,
@@ -356,6 +358,6 @@ test('calculate order price with type - take away', () => {
     macNCheese: 4
   };
 
-  const output = calculateOrderPriceWithType(menu, order, 'takeAway');
+  const output = calculateOrderPriceWithType(menu, order, "takeAway");
   expect(output).toEqual(43);
 });
